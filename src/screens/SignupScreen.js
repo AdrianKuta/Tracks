ea0ac935cc1e19ff7da8f71b5399534171e3a475
Adrian Kuta <redione1@MacBook-Pro-Adrian.local>
2020-03-18 12:35:08 +0100

    Extract AuthForm

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -3,39 +3,19 @@ import { StyleSheet, View, TouchableOpacity } from "react-native";
 import { Text, Button, Input } from "react-native-elements";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
 
 const SignupScreen = ({ navigation }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const { state, signup } = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-      </Spacer>
-      <Input
-        label="Email"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        autoCorrect={false}
+      <AuthForm
+        headerText="Sign Up for Tracker"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign Up"
+        onSubmit={signup}
       />
-      <Spacer />
-      <Input
-        label="Password"
-        value={password}
-        secureTextEntry
-        onChangeText={setPassword}
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
-      {state.errorMessage ? (
-        <Text style={styles.errorStyle}>{state.errorMessage}</Text>
-      ) : null}
-      <Spacer>
-        <Button title="Sign Up" onPress={() => signup({ email, password })} />
-      </Spacer>
       <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
         <Spacer>
           <Text style={styles.linkStyle}>
@@ -52,12 +32,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     marginBottom: 250
-  },
-  errorStyle: {
-    fontSize: 16,
-    color: "red",
-    marginLeft: 15,
-    marginTop: 15
   },
   linkStyle: {
     color: "blue"
